56723626ed14a0cf2c3fc4654c2a1fd902e846ff
cssmoyo <cssmoyo@gmail.com>
2022-12-15 12:04:59 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - Point of Entry
+ *Test whether the function input is positive/negative
+ *Return Always 0
+ */
+
+int main(void)
+{
+	int i;
+	i=0
+	positive_or_negative(i);
+
+	return(0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging, Let us fix them
