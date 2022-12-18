a0893fbb0d40d2030dd94bdfba7403f9b8ec1c3d
cssmoyo <cssmoyo@gmail.com>
2022-12-18 13:50:08 -0800

    Multiple mains

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -5,26 +5,15 @@
  * Return: 0
  */
 
-
-
-
-
 int main(void)
 
 {
-
-	 int i;
-
-
-
-	  i = 98;
-
-	   positive_or_negative(i);
-
-
-
-	    return (0);
-
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+	
+	return (0);
 }
 
 
