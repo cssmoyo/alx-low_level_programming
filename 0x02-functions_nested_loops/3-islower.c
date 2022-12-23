f28f3b0baa89798ff85b56cd17b4558d2ec507ae
cssmoyo <cssmoyo@gmail.com>
2022-12-23 11:54:37 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase character
+ * @c: character check
+ * Return: 1 for lowercase character else 0
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
+
