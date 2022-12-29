5401bbb46f3e415eaf45f7e468a82f69664f3d1a
cssmoyo <cssmoyo@gmail.com>
2022-12-29 11:30:50 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @i: number to be checked
+ * Return: 1 for upper letter or 0 for else
+ */
+
+int _isupper(int i)
+
+{
+	if (i >= 65 && i <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
