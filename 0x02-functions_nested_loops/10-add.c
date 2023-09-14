c0ea76763aaf774f99d9949f312350cec89a4598
cssmoyo <cssmoyo@gmail.com>
2023-09-14 16:56:24 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * add - adds two values
+ * @a: first value to be added
+ * @b: second value to be added
+ * Return: total
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
