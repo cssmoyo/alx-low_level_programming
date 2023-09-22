ebc7091e694a2c293bd159feaa1918ae2fe07410
cssmoyo <cssmoyo@gmail.com>
2023-09-21 18:34:18 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to be checked
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int a, b = 0;
+
+while (str[b] != '\0')
+{
+b++;
+}
+
+for (a = 0; a < b; a +=2)
+{
+putchar(str[a]);
+}
+putchar('\n');
+}
