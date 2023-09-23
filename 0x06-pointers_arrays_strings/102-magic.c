5e91a1e7b9e0ecd1b7e90282e01b4a07361acc84
cssmoyo <cssmoyo@gmail.com>
2023-09-23 06:08:16 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Add one line to this code, so that the program prints a[2] = 98
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int c;
+int f[5];
+int *x;
+
+f[2] = 1024;
+*x = &c;
+*(x + 5) = 98;
+prinft("f[2] = %d\n", f[2]);
+return (0);
+}
