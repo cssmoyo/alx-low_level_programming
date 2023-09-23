6f818c731c0d3289cbbf886ddc0f80078fc8eb5f
cssmoyo <cssmoyo@gmail.com>
2023-09-23 01:51:57 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
+ * _strcmp - compares two values of string type
  * @s1: character to be checked
  * @s2: charactor to be checked
- * Retrun: s1[a] - s2[a]
+ * Retrun: s1[a] - s2[a])
+ * Return: 0 (success)
  */
 
 int _strcmp(char *s1, char *s2)
 {
 int a = 0;
 
-while (s1[a] != '\0' &&s2[a] != '\0')
+while (s1[a] != '\0' && s2[a] != '\0')
 {
 if (s1[a] != s2[a])
 {
-return s1[a] - s2[a];
+return (s1[a] - s2[a]);
 }
 a++;
 }
