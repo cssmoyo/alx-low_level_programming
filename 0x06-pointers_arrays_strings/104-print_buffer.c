4839f67d894a83610fde458f1409bd07e6d99d6a
cssmoyo <cssmoyo@gmail.com>
2023-09-23 11:37:26 -0700

    Noise is a buffer, more effective than cubicles or booth walls

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void print_buffer -  prints a buffer
+ * @b: charactor that will act as buffer
+ * @size: buffer size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+int n = 0, d, c;
+
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+
+while (n < size)
+{
+d = size - n < 10 ? size -n : 10;
+printf("%08x:", n);
+for (c = 0; c < 10; c++)
+{
+if (c < d)
+printf("%02x", *(b + n + c));
+else
+printf(" ");
+if (c % 2)
+{
+printf(" ");
+}
+}
+for (c = 0; c < d; c++)
+{
+int e = *(b + n + c);
+
+if (e < 32 || e > 132)
+{
+e = '.';
+}
+printf("%c", e);
+}
+printf("\n");
+n += 10;
+}
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ char *rot13(char *x);
 void print_number(int n);
 void rev_string(char *x);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
+void print_buffer(char *b, int size);
+void print_buffer(char *b, int size);
 
 #endif
