6db0c5f48ec3929f20d1944e5d9f0a810a7cc9d1
cssmoyo <cssmoyo@gmail.com>
2023-09-23 16:15:05 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void simple_print_buffer(char *buffer, unsigned int size);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
